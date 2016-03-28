12224370e5aa83187ea9d2bc63ef676840182eee
Marc Plouhinec <marcplouhinec@marcs-macbook-pro.local>
2016-03-28 23:24:10 +0200

    Fix the radial gradient background where rotating the screen.

--- touroxair/ViewController.swift
+++ touroxair/ViewController.swift
@@ -9,21 +9,33 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Add a radial background
+        updateGradientBackground()
+    }
+
+    override func didRotateFromInterfaceOrientation(fromInterfaceOrientation: UIInterfaceOrientation) {
+        updateGradientBackground()
+    }
+    
+    // Add or update a radial gradient background to the view
+    func updateGradientBackground() {
         let screenCentre = CGPointMake(self.view.frame.size.width / 2, self.view.frame.size.height/2)
         let innerColour = UIColor(red: 102/255, green: 102/255, blue: 102/255, alpha: 1.0).CGColor
         let outterColour = UIColor.blackColor().CGColor
         let radialGradientBackground = RadialGradientLayer(center: screenCentre, radius: CGFloat(self.view.frame.size.width * 0.9), colors: [innerColour, outterColour])
         radialGradientBackground.frame = self.view!.bounds
-        self.view!.layer.insertSublayer(radialGradientBackground, atIndex: 0)
+        if let sublayers = self.view!.layer.sublayers where !sublayers.isEmpty && sublayers[0] is RadialGradientLayer {
+            self.view!.layer.replaceSublayer(sublayers[0], with: radialGradientBackground)
+        }
+        else {
+            self.view!.layer.insertSublayer(radialGradientBackground, atIndex: 0)
+        }
+
         radialGradientBackground.setNeedsDisplay()
     }
-
-
 
 }
 
