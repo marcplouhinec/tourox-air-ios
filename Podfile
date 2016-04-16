503b2764d8e725983c1bb870bdcbb22132c97d80
Marc Plouhinec <m_plouhinec@yahoo.fr>
2016-04-16 15:33:14 +0200

    Get rid of LibLinphone (because it doesn't work with the Asterisk ConfBridge app) and replace it by PJSIP. Also handle dependencies thanks to CocoaPods.

---
+++ Podfile
@@ -0,0 +1,7 @@
+platform :ios, '7.0'
+use_frameworks!
+
+target 'touroxair' do
+	pod 'pjsip', '~> 2.3.0.7'
+end
+
---
+++ Pods/Target Support Files/Pods-touroxair/Pods-touroxair-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_touroxair : NSObject
+@end
+@implementation PodsDummy_Pods_touroxair
+@end
